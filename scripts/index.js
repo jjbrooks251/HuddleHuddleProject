d33f5dc289fe8a7d602c0918c30017a5925cfe96
Harry Buck <harry-buck@hotmail.co.uk>
2019-06-13 14:03:48 +0100

    Added functionality to view pets for a specific owner

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,10 @@
 let records = []; //All records currently pulled from the database
-let selectedRecord; //The currently selected record (if any)
+//let selectedRecord; //The currently selected record (if any)
 
-const selectRecord = (id) => {
+const getRecord = (id) => {
     for(let record of records){
         if(record.id == id){
-            selectedRecord = record;
+            return record;
         }
     }
 };
@@ -72,10 +72,75 @@ function displayData(data) {
                 selector.setAttribute("class", "btn")
                 selector.setAttribute("onclick", "viewOwnerPets(id)");
                 selector.value = "View Pets"
+                selector.setAttribute("data-toggle", "modal");
+                selector.setAttribute("data-target", "#myModal");
                 cell.append(selector);
             }
         }
     });
+}
+
+
+function viewOwnerPets(ownerId){
+    const ownerPets = getRecord(ownerId).pets;    
+    const modalBody = document.getElementById("modal-body");
+
+    let dataTable = document.getElementById("modal-tbl");
+    let keys = Object.keys(ownerPets[0]); //Get all of the keys for the object. We dont care about which object.
+
+    //If the data table exists then delete it by removing it from the parent
+    if (!!dataTable) {
+        modalBody.removeChild(dataTable);
+    }
+
+    //Set up the initial table
+    dataTable = document.createElement("table");
+    dataTable.className = "table table-hover"; //Bootstrap
+    dataTable.id = "modal-tbl";
+    let head = dataTable.createTHead();
+    head.className = "thead-dark"; //Bootstrap        
+
+    //Create table headers    
+    for (let key of keys) {
+        let cell = document.createElement("th");
+        cell.innerHTML = "<b>" + key + "</b>";
+        head.appendChild(cell);
+    }
+
+    //Create table header cell for the selection boxes
+    let headCell = document.createElement("th");
+    headCell.innerHTML = "<b>Actions</b>";
+    head.appendChild(headCell);
+
+    //Attach the table to the document
+    modalBody.appendChild(dataTable);
+
+    //Start appending the data
+    let body = dataTable.createTBody();
+
+    //Create table rows
+    ownerPets.forEach(element => {
+        row = body.insertRow();
+
+        for (let i = 0; i < keys.length + 1; i++) {
+            let cell = row.insertCell();
+            console.log(element);
+
+            if (i < keys.length) {
+                let text = document.createTextNode(element[keys[i]]);
+                cell.append(text);
+            } else {
+                let selector = document.createElement("INPUT");
+                selector.setAttribute("type", "button");
+                selector.setAttribute("id", element[keys[0]])
+                selector.setAttribute("class", "btn")
+                selector.setAttribute("onclick", "viewPetVists(id)");
+                selector.value = "View Visits"
+                cell.append(selector);
+            }
+        }
+    });
+
 }
 
 function makeRequest(method, url, body) {
